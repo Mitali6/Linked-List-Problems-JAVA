e907f4926f57744224633da99c7c500c6cd6348b
Saxena <mitali.saxena@gartner.com>
2022-05-17 00:58:20 +0530

    Linked List Deletion and finding the middle node.

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,10 +1,10 @@
 package LinkedList;
 
-public class LinkedListInsertion {
+public class LinkedList {
 	
 	Node head;
 	
-	public LinkedListInsertion() {
+	public LinkedList() {
 		head = null; 
 	}
 	
@@ -14,7 +14,7 @@ public class LinkedListInsertion {
 			System.out.print(temp.data + " ");
 			temp = temp.next;
 		}
-		System.out.println();
+		System.out.println("\n");
 	}
 	
 	public void insertAtBeginning(int key) {
@@ -36,10 +36,10 @@ public class LinkedListInsertion {
 	}
 
 	public void insertInBetween(int insertAfterKey, int key) {
-		Node temp = head;
 		if(head == null) {
 			System.out.println("Node can't be inserted as Linked List is empty");
 		} else {
+			Node temp = head;
 			while(temp != null && temp.data != insertAfterKey) {
 				temp = temp.next;
 			}
@@ -51,6 +51,42 @@ public class LinkedListInsertion {
 				System.out.println("Node doesn't exist");
 			}
 		}
+	}
+	
+	public void deleteNode(int keyToBeDeleted) {
+		if(head == null) {
+			System.out.println("Node can't be deleted as Linked List is empty");
+		} else {
+			Node temp = head, prev = null;
+			if(temp.data == keyToBeDeleted) {
+				head = temp.next;
+				return;
+			}
+			while(temp != null && temp.data != keyToBeDeleted) {
+				prev = temp;
+				temp = temp.next;
+			}
+			if(temp == null) {
+				System.out.println("Node to be deleted doesn't exist");
+				return;
+			} 
+			prev.next = temp.next;
+		}
+	}
+
+	public int findMiddleNodeOfLinkedList(Node head) {
+		
+		if(head == null) {
+			System.out.println("Linked List is empty");
+			return -1;
+		}
+		
+		Node slow = head, fast = head;
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow.data;
 	}
 
 }
--- src/LinkedList/MainLL.java
+++ src/LinkedList/MainLL.java
@@ -4,20 +4,63 @@ public class MainLL {
 
 	public static void main(String[] args) {
 		
-		LinkedListInsertion list = new LinkedListInsertion();
-		list.head = new Node(2);
-		list.head.next = new Node(3);
-		list.head.next.next = new Node(4);
-		System.out.println("Initial list:");
+		LinkedList list = new LinkedList();
+		
+		list.insertInBetween(4, 5);
+		System.out.println("List after trying to insert a node when linked list is empty");
 		list.printLinkedList();
+		
+		list.deleteNode(7);
+		System.out.println("List after attempting to delete a node when linked list is empty");
+		list.printLinkedList();
+		
+		list.insertAtEnd(3);
+		System.out.println("List after inserting a new node at end:");
+		list.printLinkedList();
+		
+		list.insertAtBeginning(2);
+		System.out.println("List after inserting a new node at beginning:");
+		list.printLinkedList();
+
+		list.insertAtEnd(4);
+		System.out.println("List after inserting a new node at end:");
+		list.printLinkedList();
+		
+		
 		list.insertAtBeginning(1);
 		System.out.println("List after inserting a new node at beginning:");
 		list.printLinkedList();
+		
 		list.insertAtEnd(6);
 		System.out.println("List after inserting a new node at end:");
 		list.printLinkedList();
+		
+		System.out.println("The middle node of given linked list with even number of nodes has key " + list.findMiddleNodeOfLinkedList(list.head) + "\n");
+		
 		list.insertInBetween(4, 5);
 		System.out.println("List after inserting a new node in between:");
+		list.printLinkedList();
+		
+		System.out.println("The middle node of given linked list with odd number of nodes has key " + list.findMiddleNodeOfLinkedList(list.head) + "\n");
+		
+		list.insertInBetween(7, 8);
+		System.out.println("List after trying to insert a node after a node which does not exist");
+		list.printLinkedList();
+		
+		list.deleteNode(1);
+		System.out.println("List after deleting a node which is at beginning");
+		list.printLinkedList();
+		
+		list.deleteNode(3);
+		System.out.println("List after deleting a node in between");
+		list.printLinkedList();
+		
+		list.deleteNode(6);
+		System.out.println("List after deleting a node at end");
+		list.printLinkedList();
+		
+		list.deleteNode(7);
+		System.out.println("List after attempting to delete a node that does not exist");
 		list.printLinkedList();
 		
 	}
